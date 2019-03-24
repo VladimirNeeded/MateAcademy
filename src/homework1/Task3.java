2dbd45626ad8bc23ca3b8d7a0d72ac233029ef2e
VladimirNeeded <47814504+VladimirNeeded@users.noreply.github.com>
2019-03-24 11:11:46 +0200

    Update Task3.java

--- src/homework1/Task3.java
+++ src/homework1/Task3.java
@@ -2,7 +2,7 @@ package homework1;
 
 public class Task3 {
     public static void main(String[] args) {
-    System.out.println(Power(2,1));
+        System.out.println(Power(2,1));
     }
     public static int Power (int num, int pow){
         int x = num;
